e9108fab1130785f4329b3c306b89889ecd40dab
Jeffrey Fate <jeffreyfate@wisercare.com>
2019-10-17 15:51:54 -0700

    fix(4): I suck at paths. Resolves https://github.com/jeffreyfate/aws-elb-lib/issues/4

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const {repackageArrayParam} = require('./src/lib/aws/request/repackage-array-param')
-const {elbDescribeTargetGroups} = require('./src/aws/elb')
-const {extractArns} = require('./src/lib/aws/target-group/arn/extract-from-response')
-const {getTargetGroupArns} = require('./src/mod/get-target-group-arns')
+const {repackageArrayParam} = require('./lib/aws/request/repackage-array-param')
+const {elbDescribeTargetGroups} = require('./aws/elb')
+const {extractArns} = require('./lib/aws/target-group/arn/extract-from-response')
+const {getTargetGroupArns} = require('./mod/get-target-group-arns')
 
 exports.getTargetGroupArns = async function(names) {
   return getTargetGroupArns(names, repackageArrayParam, elbDescribeTargetGroups, extractArns)
