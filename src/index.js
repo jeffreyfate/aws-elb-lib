8d80d05f38efd07cda38eb9f7f320310e1c63e7d
Jeffrey Fate <jeffreyfate@wisercare.com>
2019-10-17 15:48:34 -0700

    fix(4): Resolves https://github.com/jeffreyfate/aws-elb-lib/issues/4

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-const appRoot = require('app-root-path');
-const {repackageArrayParam} = require(appRoot + '/src/lib/aws/request/repackage-array-param')
-const {elbDescribeTargetGroups} = require(appRoot + '/src/aws/elb')
-const {extractArns} = require(appRoot + '/src/lib/aws/target-group/arn/extract-from-response')
-const {getTargetGroupArns} = require(appRoot + '/src/mod/get-target-group-arns')
+const {repackageArrayParam} = require('./src/lib/aws/request/repackage-array-param')
+const {elbDescribeTargetGroups} = require('./src/aws/elb')
+const {extractArns} = require('./src/lib/aws/target-group/arn/extract-from-response')
+const {getTargetGroupArns} = require('./src/mod/get-target-group-arns')
 
 exports.getTargetGroupArns = async function(names) {
   return getTargetGroupArns(names, repackageArrayParam, elbDescribeTargetGroups, extractArns)
